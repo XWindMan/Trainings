aed7b50ae6dcb40c5cf7710ab108763b213a1947
tony <tony.xia@vtrump.com>
2017-07-10 15:08:02 +0800

    recycleview+横向滑动+渐变数据图

--- FragmentNestDemo/src/main/java/com/vtrump/fragmentnestdemo/MonthDataView.java
+++ FragmentNestDemo/src/main/java/com/vtrump/fragmentnestdemo/MonthDataView.java
@@ -3,10 +3,11 @@ package com.vtrump.fragmentnestdemo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -21,11 +22,12 @@ public class MonthDataView extends View {
     private Paint mTextPaint;
 
     private float interval = 50;
-    private float lineHeight = 100;
     private float mPadding = 30;
-    private int days = 30;
 
+    private String startColor = "#ffffff";
+    private String endColor = "#FF8C00";
     private List<Float> data;
+    private List<String> textList;
 
     public MonthDataView(Context context) {
         this(context, null);
@@ -35,12 +37,13 @@ public class MonthDataView extends View {
         super(context, attrs);
         initPaint();
         data = new ArrayList<>();
+        textList = new ArrayList<>();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension((int) (interval * days + 2 * mPadding), 200);
+        setMeasuredDimension((int) (interval * data.size() + 2 * mPadding), 200);
     }
 
     private void initPaint() {
@@ -61,7 +64,7 @@ public class MonthDataView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.translate(0, getHeight() * 2 / 3);
-        if (data.size() == 0) {
+        if (data.size() == 0 || textList.size() == 0) {
             return;
         }
         drawlines(canvas);
@@ -69,24 +72,26 @@ public class MonthDataView extends View {
     }
 
     private void drawlines(Canvas canvas) {
-        float startX = 0, startY = 0, endX, endY;
+        float startX = 0, startY = 0, endX, endY = 0;
         for (int i = 0; i < data.size(); i++) {
             startX = mPadding + i * interval;
             endX = startX;
             startY = 0;
             endY = -data.get(i) * (getHeight() * 2 / 3 - mPadding);
-            Log.d(TAG, "drawlines: endY=" + endY + ",height=" + getHeight() * 2 / 3);
+            LinearGradient gradient = new LinearGradient(startX, startY, endX, endY,
+                    Color.parseColor(startColor), Color.parseColor(endColor), Shader.TileMode.CLAMP);
+            mDataPaint.setShader(gradient);
             canvas.drawLine(startX, startY, endX, endY, mDataPaint);
         }
     }
 
     private void drawTexts(Canvas canvas) {
         float x = 0, y = 0;
-        for (int i = 0; i < days; i++) {
+        for (int i = 0; i < textList.size(); i++) {
             x = mPadding + i * interval;
             y = getHeight() / 6 + mPadding;
-            float tw = mTextPaint.measureText(i + "");
-            canvas.drawText(i + "", x - tw / 2, y, mTextPaint);
+            float tw = mTextPaint.measureText(textList.get(i));
+            canvas.drawText(textList.get(i), x - tw / 2, y, mTextPaint);
         }
     }
 
@@ -94,7 +99,29 @@ public class MonthDataView extends View {
         return data;
     }
 
-    public void setData(List<Float> data) {
+    public MonthDataView setData(List<Float> data) {
         this.data = data;
+        return this;
     }
+
+    public List<String> getTextList() {
+        return textList;
+    }
+
+    public MonthDataView setTextList(List<String> textList) {
+        this.textList = textList;
+        return this;
+    }
+
+    public MonthDataView setStartColor(String startColor) {
+        this.startColor = startColor;
+        return this;
+    }
+
+
+    public MonthDataView setEndColor(String endColor) {
+        this.endColor = endColor;
+        return this;
+    }
+
 }
--- FragmentNestDemo/src/main/java/com/vtrump/fragmentnestdemo/PlueThreeFragment.java
+++ FragmentNestDemo/src/main/java/com/vtrump/fragmentnestdemo/PlueThreeFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,8 +32,7 @@ public class PlueThreeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_plue_three, container, false);
-        MonthDataView m = (MonthDataView) view.findViewById(R.id.monthDataView);
-        m.setData(getData());
+
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         LinearLayoutManager manager = new LinearLayoutManager(getActivity());
         manager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -44,13 +42,48 @@ public class PlueThreeFragment extends Fragment {
         return view;
     }
 
-    private List<Float> getData() {
-        List<Float> data = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            data.add((float) (Math.random()));
-            Log.d(TAG, "getData: " + data.get(i));
+    private List<List<Float>> getData() {
+        List<List<Float>> list = new ArrayList<>();
+        List<Float> data1 = new ArrayList<>();
+        for (int i = 0; i < 30; i += 2) {
+            data1.add((float) (Math.random()));
         }
-        return data;
+        List<Float> data2 = new ArrayList<>();
+        for (int i = 0; i < 14; i += 2) {
+            data2.add((float) (Math.random()));
+        }
+        List<Float> data3 = new ArrayList<>();
+        for (int i = 0; i < 24; i += 2) {
+            data3.add((float) (Math.random()));
+        }
+        list.add(data1);
+        list.add(data2);
+        list.add(data3);
+        return list;
+    }
+
+    private List<List<String>> getTextData() {
+        List<List<String>> list = new ArrayList<>();
+        List<String> data1 = new ArrayList<>();
+        for (int i = 0; i < 30; i += 2) {
+            data1.add(i + "");
+        }
+        List<String> data2 = new ArrayList<>();
+        data2.add("M");
+        data2.add("T");
+        data2.add("W");
+        data2.add("T");
+        data2.add("F");
+        data2.add("S");
+        data2.add("S");
+        List<String> data3 = new ArrayList<>();
+        for (int i = 0; i < 24; i += 2) {
+            data3.add(i + "");
+        }
+        list.add(data1);
+        list.add(data2);
+        list.add(data3);
+        return list;
     }
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
@@ -69,7 +102,8 @@ public class PlueThreeFragment extends Fragment {
         //将数据与界面进行绑定的操作
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, int position) {
-            viewHolder.mMonthDataView.setData(getData());
+            viewHolder.mMonthDataView.setData(getData().get(position));
+            viewHolder.mMonthDataView.setTextList(getTextData().get(position));
         }
 
         //获取数据的数量
@@ -88,5 +122,4 @@ public class PlueThreeFragment extends Fragment {
             }
         }
     }
-
 }
